e56f8140d8be7ba73ea8a84655d0d976fb1d897f
Roie Koper <roiekoper@gmail.com>
2015-08-06 08:15:10 +0300

    test

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,27 @@ class EventsController < ApplicationController
   end
 
   def create
-    event = Event.create(params.permit(:title, :description, :start_date, :end_date, :location))
+    attrs = params.permit(:title, :description,
+                          :start_date, :end_date,
+                          :end_time,:start_time,
+                          :location)
+
+    p DateTime.strptime("#{params[:start_date].strftime('%Y-%m-%d')} #{"#{Time.parse(params[:start_time])} +02:00".to_time.strftime('%H:%M:%S')}",
+                        '%Y-%m-%d %H:%M:%S')
+    p DateTime.strptime("#{params[:end_date].strftime('%Y-%m-%d')} #{"#{Time.parse(params[:end_time])} +02:00".to_time.strftime('%H:%M:%S')}",
+                        '%Y-%m-%d %H:%M:%S')
+
+    attrs.slice(:title,:description,:location).
+        merge(:start_date => DateTime.strptime("#{params[:start_date].strftime('%Y-%m-%d')} #{"#{Time.parse(params[:start_time])} +02:00".to_time.strftime('%H:%M:%S')}",
+                                               '%Y-%m-%d %H:%M:%S'),
+              :end_date => DateTime.strptime("#{params[:end_date].strftime('%Y-%m-%d')} #{"#{Time.parse(params[:end_time])} +02:00".to_time.strftime('%H:%M:%S')}",
+                                               '%Y-%m-%d %H:%M:%S'))
+
+
+    event = Event.create(params.permit(:title, :description,
+                                       :start_date, :end_date,
+                                       :end_time,:start_time,
+                                       :location))
 
     if event.errors.empty?
       EventsUser.create(:user_id => current_user.id,
