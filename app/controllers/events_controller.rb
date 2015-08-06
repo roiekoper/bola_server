3a331fa8c927a83ca7cde487cdc2bfc1d9973b0b
Roie Koper <roiekoper@gmail.com>
2015-08-06 08:42:16 +0300

    Fix create event.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,34 +11,11 @@ class EventsController < ApplicationController
                           :end_time, :start_time,
                           :location)
 
-    p '============================='
-    p '============================='
-    p '============================='
-    p DateTime.strptime("#{(Time.parse(params[:start_date]) + 1.days).strftime('%Y-%m-%d')} #{"#{Time.parse(params[:start_time]) + 2.hours}".to_time.strftime('%H:%M:%S')}",
-                        '%Y-%m-%d %H:%M:%S')
-    p '+++++++++++++++'
-    p DateTime.strptime("#{(Time.parse(params[:end_date]) + 1.days).strftime('%Y-%m-%d')} #{"#{Time.parse(params[:end_time]) + 2.hours}".to_time.strftime('%H:%M:%S')}",
-                        '%Y-%m-%d %H:%M:%S')
-
-    p '============================='
-    p '============================='
-    p '============================='
-
-    p attrs.slice(:title, :description, :location).
-          merge(:start_date => DateTime.strptime("#{(Time.parse(params[:start_date]) + 1.days).strftime('%Y-%m-%d')} #{"#{Time.parse(params[:start_time]) + 2.hours}".to_time.strftime('%H:%M:%S')}",
-                                                 '%Y-%m-%d %H:%M:%S'),
-                :end_date => DateTime.strptime("#{(Time.parse(params[:end_date]) + 1.days).strftime('%Y-%m-%d')} #{"#{Time.parse(params[:end_time]) + 2.hours}".to_time.strftime('%H:%M:%S')}",
-                                               '%Y-%m-%d %H:%M:%S'))
-
-    p '============================='
-    p '============================='
-    p '============================='
-
-
-    event = Event.create(params.permit(:title, :description,
-                                       :start_date, :end_date,
-                                       :end_time, :start_time,
-                                       :location))
+    event = Event.create(attrs.slice(:title, :description, :location).
+                             merge(:start_date => DateTime.strptime("#{(Time.parse(params[:start_date]) + 1.days).strftime('%Y-%m-%d')} #{"#{Time.parse(params[:start_time]) + 2.hours}".to_time.strftime('%H:%M:%S')}",
+                                                                    '%Y-%m-%d %H:%M:%S'),
+                                   :end_date => DateTime.strptime("#{(Time.parse(params[:end_date]) + 1.days).strftime('%Y-%m-%d')} #{"#{Time.parse(params[:end_time]) + 2.hours}".to_time.strftime('%H:%M:%S')}",
+                                                                  '%Y-%m-%d %H:%M:%S')))
 
     if event.errors.empty?
       EventsUser.create(:user_id => current_user.id,
